21e5f68d8356c2c0b3ed93e078f523356726d3e4
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-11-09 12:10:56 -0600

    Agregando validaciones useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
-export const useForm = <T>(initialState: T) => {
+export const useForm = <T, E>(
+  initialState: T,
+  initialError?: E,
+  fnValidation?: (state: E, event: React.ChangeEvent<HTMLInputElement>) => E
+) => {
+
   const [formData, setFormdata] = useState(initialState);
+  const [errorForm, setErrorForm] = useState(initialError);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -12,15 +18,27 @@ export const useForm = <T>(initialState: T) => {
     }));
   };
 
+  const handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setErrorForm(prevState => fnValidation && fnValidation(prevState!, event));
+  };
+
+  const resetForm = () => {
+    setFormdata(initialState);
+    setErrorForm(initialError);
+  };
+
   const newPropsInput = (name: string) => ({
     name,
     value: (formData as any)[name],
-    onChange: handleChange
+    onChange: handleChange,
+    onBlur: handleBlur
   });
 
   return {
     formData,
+    errorForm,
     handleChange,
-    newPropsInput
+    newPropsInput,
+    resetForm
   };
 };
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -8,10 +8,48 @@ const initialState = {
   password2: ''
 };
 
-const RegisterPage = () => {
-  const { formData, newPropsInput } = useForm(initialState);
+const initialError = {
+  name: {
+    isError: false,
+    message: ''
+  },
+  email: {
+    isError: false,
+    message: ''
+  },
+  password1: {
+    isError: false,
+    message: ''
+  },
+  password2: {
+    isError: false,
+    message: ''
+  }
+};
 
-  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+const RegisterPage = () => {
+  const { formData, errorForm, newPropsInput, resetForm } = useForm(initialState, initialError, formValidation);
+
+  function formValidation(state = initialError, event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    let valueError = {
+      isError: false,
+      message: ''
+    };
+
+    if (value.trim().length <= 0) {
+      valueError.isError = true;
+      valueError.message = 'El valor es requerido';
+    }
+
+    return {
+      ...state,
+      [name]: valueError
+    };
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("state: ", formData);
   };
@@ -20,36 +58,50 @@ const RegisterPage = () => {
     <div>
       <h1>Register page</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="name"
+          className={errorForm?.name.isError ? 'has-error' : ''}
           {...newPropsInput('name')}
         />
+        {errorForm?.name.isError && <span>{errorForm?.name.message}</span>}
 
         <input
           type="email"
           placeholder="email"
+          className={errorForm?.email.isError ? 'has-error' : ''}
           {...newPropsInput('email')}
         />
+        {errorForm?.email.isError && <span>{errorForm?.email.message}</span>}
 
         <input
           type="password"
           placeholder="password"
+          className={errorForm?.password1.isError ? 'has-error' : ''}
           {...newPropsInput('password1')}
         />
+        {errorForm?.password1.isError && <span>{errorForm?.password1.message}</span>}
 
         <input
           type="password"
           placeholder="repeat password"
+          className={errorForm?.password2.isError ? 'has-error' : ''}
           {...newPropsInput('password2')}
         />
+        {errorForm?.password2.isError && <span>{errorForm?.password2.message}</span>}
 
         <button
           type="submit"
-          onClick={handleSubmit}
         >
           Create
+        </button>
+
+        <button
+          type="button"
+          onClick={resetForm}
+        >
+          Restet
         </button>
       </form>
     </div>
